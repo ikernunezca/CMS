978f7de30cb866aa65e238784082840e07d872da
Iker Núñez Carpintero <48946661+ikernunezca@users.noreply.github.com>
2019-10-07 10:10:48 +0200

    Update Fisher_CMS.R

--- Scripts/Fisher_CMS.R
+++ Scripts/Fisher_CMS.R
@@ -41,7 +41,7 @@ res <- sapply(c,function(x) fisherP(x))
 names(res) <- colnames(tab)[c]
 res <- sort(res)
 
-pdf('Fisher_CMS.pdf',width=10)
+pdf('~/Plots/Fisher_CMS.pdf',width=10)
 op <- par(mar=c(15,4,4,4),ps=18)
 bp <- barplot(res,ylab='p-value',names='')
 abline(h=0.05,lty='dashed')
